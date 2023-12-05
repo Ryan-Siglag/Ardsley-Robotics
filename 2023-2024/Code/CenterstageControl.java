0d07b097d49a59402800e962e5081920c04b2d68
Ryan-Siglag <Ryan.Siglag@gmail.com>
2023-12-05 10:55:15 -0500

    Add Centerstage Control

---
+++ 2023-2024/Code/CenterstageControl.java
@@ -0,0 +1,126 @@
+/*
+Copyright 2022 FIRST Tech Challenge Team 10870
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial
+portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.Gyroscope;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.Blinker;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
+ * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
+ * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a PushBot
+ * It includes all the skeletal structure that all linear OpModes contain.
+ *
+ * Remove a @Disabled the on the next line or two (if present) to add this opmode to the Driver Station OpMode list,
+ * or add a @Disabled annotation to prevent this OpMode from being added to the Driver Station
+ */
+@TeleOp
+
+public class PowerPlayControl extends LinearOpMode {
+    private Blinker control_Hub;
+    private Blinker expansion_Hub;
+    private DcMotor backLeft;
+    private DcMotor backRight;
+    private DcMotor frontLeft;
+    private DcMotor frontRight;
+    private DcMotor armPitch;
+    private Servo thumb;
+    
+    double drivePower = 0.7;
+    double pitchPower = 0.3;
+    
+    @Override
+    public void runOpMode() {
+        control_Hub = hardwareMap.get(Blinker.class, "Control Hub");
+        expansion_Hub = hardwareMap.get(Blinker.class, "Expansion Hub");
+        
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        
+        armPitch = hardwareMap.get(DcMotor.class, "armPitch");
+        thumb = hardwareMap.get(Servo.class, "thumb");
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            telemetry.addData("Status", "Running");
+
+            double y = -gamepad1.left_stick_y; // Remember, this is reversed!
+            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+            double rx = gamepad1.right_stick_x; //Need gamepad1 statement?
+
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio, but only when
+            // at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (y + x + rx) / denominator;
+            double backLeftPower = (y - x + rx) / denominator;
+            double frontRightPower = (y - x - rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
+
+            frontLeft.setPower(frontLeftPower*drivePower);
+            backLeft.setPower(backLeftPower*drivePower);
+            frontRight.setPower(frontRightPower*drivePower);
+            backRight.setPower(backRightPower*drivePower);
+            
+            
+            
+            if (this.gamepad1.left_bumper) { //Need "this"?
+                armPitch.setPower(pitchPower);
+                telemetry.addData("Arm moving", "Down");
+            } else if (this.gamepad1.right_bumper){
+                armPitch.setPower(-pitchPower);
+                telemetry.addData("Arm moving", "Up");
+            } else {
+                pulley.setPower(0);
+                telemetry.addData("Pully moving", "Stopped");
+            }
+            
+            if (this.gamepad1.right_trigger > 0.0) {
+                thumb.setPosition(1.0);
+                telemetry.addData("Thumb", "Open");
+            } else if (this.gamepad1.left_trigger > 0.0) {
+                thumb.setPosition(0.0);
+                telemetry.addData("Thumb", "Closed");
+            }
+            
+            telemetry.update();
+        }
+    }
+}
